bed644faa21faa8303e1c458693d3033640069cc
Kyle Jacobson <jacokyle01@gmail.com>
2024-01-07 00:08:08 -0600

    serve html file with image

---
+++ lol.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.get('/', (req, res) => {
+    res.send('lol');
+});
+
+app.get('/biden', (req, res) => {
+    const filePath = path.join(__dirname, 'lol.html');
+    res.sendFile(filePath);
+});
+
+
+const PORT = 8080; // You can choose any available port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
