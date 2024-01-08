5dd0b12916e348ca5a8d10b36a9507bb2286f2ae
Kyle Jacobson <jacokyle01@gmail.com>
2024-01-07 18:11:31 -0600

    add test route /test

--- lol.js
+++ lol.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "public", "lol.html"));
 });
 
+app.get("/test", (req, res) => {
+    res.send("test");
+});
+
 const PORT = 8080; // You can choose any available port
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
